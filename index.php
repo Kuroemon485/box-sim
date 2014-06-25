89f96d378be44d0f86936b03ee5a25c5de4e624e
Wild cat <Kuroemon485@gmail.com>
2014-06-25 12:46:39 +0700

    Master

---
+++ index.php
@@ -0,0 +1,207 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Box Simulator | CompetitionDB</title>
+    <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <link href="public/img/favicon.ico" type="image/x-icon" rel="shortcut icon">
+    <!-- bootstrap 3.0.2 -->
+    <link rel="stylesheet" href="public/css/bootstrap.min.css">
+    <!-- font Awesome -->
+    <link href="public/css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
+    <!-- Ionicons -->
+    <link href="public/css/ionicons.min.css" rel="stylesheet" type="text/css"/>
+    <!-- Theme style -->
+    <link href="public/css/AdminLTE.css" rel="stylesheet" type="text/css"/>
+    <link href="public/css/cdb.css" rel="stylesheet" type="text/css" />
+    
+<!--    UPLOAD END-->
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body class="skin-blue fixed">
+<!-- header logo: style can be found in header.less -->
+<header class="header">
+    <a href="/" class="logo">
+        <!-- Add the class icon to your logo image or logo icon to add the margining -->
+        Competition<span class="text-green">DB</span>
+    </a>
+    <!-- Header Navbar: style can be found in header.less -->
+    <nav class="navbar navbar-static-top" role="navigation">
+        <!-- Sidebar toggle button-->
+        <a href="#" class="navbar-btn sidebar-toggle" data-toggle="offcanvas" role="button">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </a>
+        <div class="fb-like" data-href="competitiondb.io" data-layout="button_count" data-action="like" data-show-faces="false" data-share="true" style="margin-top: 12px; margin-left: 65px;"></div>
+    </nav>
+
+</header>
+<div class="wrapper row-offcanvas row-offcanvas-left">
+    <!-- Left side column. contains the logo and sidebar -->
+    <aside class="left-side sidebar-offcanvas">
+        <!-- sidebar: style can be found in sidebar.less -->
+        <section class="sidebar">
+            <!-- sidebar menu: : style can be found in sidebar.less -->
+            <!-- <form action="#" method="get" class="sidebar-form">
+                <div class="input-group">
+                    <input type="text" name="q" class="form-control" placeholder="Search..."/>
+                    <span class="input-group-btn">
+                        <button type='submit' name='seach' id='search-btn' class="btn btn-flat"><i class="fa fa-search"></i></button>
+                    </span>
+                </div>
+            </form> -->
+            <ul class="sidebar-menu">
+                <li class="active treeview">
+                    <a href="#">
+                        <i class="fa fa-briefcase"></i> <span>Box Simulator</span>
+                    </a>
+                </li>
+            </ul>
+            <span class="col-md-12" style="text-align: center;">
+                <p class="text-muted">©<?php echo '2014 - ' . date('Y'); ?> Wildcat</p>
+            </span>
+        </section>
+        <!-- /.sidebar -->
+    </aside>
+
+<aside class="right-side">
+<!-- Content Header (Page header) -->
+<section class="content-header">
+    <h1>
+        Pokemon X/Y Tools
+    </h1>
+    <ol class="breadcrumb">
+        <li><a href="#">X/Y Tools</a></li>
+        <li class="active">Boxes Simulator</li>
+    </ol>
+</section>
+<!-- Main content -->
+<section class="content">
+	<div class="row">
+	 	<div class="col-md-12" id="">
+	        <!-- Primary box -->
+	        <div class="nav-tabs-custom">
+                <ul class="nav nav-tabs pull-right">
+                    <li class="active"><a href="#box-data" data-toggle="tab">Box Simulator</a></li>
+                    <li class="pull-left header"><button class="btn btn-sm btn-success" id="cfm"><i class="fa fa-question"></i></button></li>
+                </ul>
+                <div class="tab-content">
+                    <div class="tab-pane active" id="box-data">
+                        <div class="row">
+                            
+                        </div>
+                        <div class="row">
+                            <div class="col-md-6 col-sm-offset-3">
+                            
+                            <!-- Success box -->
+                                <div class="box box-success">
+                                    <div class="box-header">
+                                        <h3 class="box-title">Box 1</h3>
+                                        <div class="box-tools pull-right">
+                                            <div class="btn-group">
+                                                <div class="btn-group">
+                                                    <button class="btn btn-success btn-sm switch-box to-prev-box" data-to-box="#B30"><i class="fa fa-angle-left"></i></button>
+                                                    <button type="button" class="btn btn-success btn-sm box-num dropdown-toggle" data-toggle="dropdown">#B1</button>
+                                                    <button class="btn btn-success btn-sm switch-box to-next-box" data-to-box="#B2"><i class="fa fa-angle-right"></i></button>
+                                                    <ul class="dropdown-menu box-list" style="max-height: 300px; overflow-y: scroll;">
+                                                        <?php for ($bl=1; $bl < 31; $bl++): ?>
+                                                            <li><a href="#B<?php echo $bl ?>">Box <?php echo $bl ?></a></li>
+                                                        <?php endfor ?>
+                                                    </ul>
+                                                </div>
+                                            </div>
+                                            <button class="btn btn-success btn-sm" id="cim">Import</button>
+                                        </div>
+                                    </div>
+                                    <div class="box-body">
+                                    <?php for ($b=1; $b < 31; $b++): ?>
+                                        <table class="table poke-box" <?php if ($b >= 2) echo 'style="display: none"' ?> id="B<?php echo $b ?>">
+                                            <tbody>
+                                            <?php for ($r=1; $r < 6; $r++): ?>
+                                                <tr>
+                                                <?php for ($c=1; $c < 7; $c++): ?>
+                                                    <td id="<?php echo $r."-".$c ?>" style="text-align: center;"><button id="" class="btn btn-default show-info" data-container="body" data-toggle="popover" data-placement="top" data-content="Empty slot"><img src="public/images/favicon.ico" alt=""></button></td>
+                                                <?php endfor ?>
+                                                </tr>
+                                            <?php endfor ?>
+                                            </tbody>
+                                        </table>
+                                    <?php endfor ?>
+                                    </div><!-- /.box-body -->
+                                </div><!-- /.box -->
+                            </div><!-- /.col -->
+                        </div>
+                    </div>
+
+                    </div><!-- /.tab-pane -->
+                    <div class="tab-pane" id="predict">
+                        
+                    </div><!-- /.tab-pane -->
+                </div><!-- /.tab-content -->
+            </div><!-- nav-tabs-custom -->
+	    </div><!-- /.col -->	</div>
+	<div class="modal fade" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true" id="import-modal">
+		<div class="modal-dialog modal-md">
+			<div class="modal-content">
+				<div class="modal-header">
+					<button type="button" class="pull-right btn btn-xs btn-danger" data-dismiss="modal" aria-hidden="true"><i class="fa fa-times"></i></button>
+					<h4 class="modal-title" id="notice-title">Raw Data</h4>
+				</div>
+				<div class="modal-body" id="">
+                    <textarea name="" id="" cols="30" rows="10" class="form-control"></textarea>
+				</div>
+                <div class="modal-footer">
+                    <button class="btn btn-sm btn-success" id="impd">Import</button>
+                    <button type="button" class="btn btn-sm btn-danger" data-dismiss="modal" aria-hidden="true"><i class="fa fa-times"></i>
+                    Close</button>
+                </div>
+			</div>
+		</div>
+	</div>
+    <div class="modal fade" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true" id="faqs-modal">
+        <div class="modal-dialog modal-md">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="pull-right btn btn-xs btn-danger" data-dismiss="modal" aria-hidden="true"><i class="fa fa-times"></i></button>
+                    <h4 class="modal-title" id="notice-title">How to use</h4>
+                </div>
+                <div class="modal-body" id="">
+                    <p>
+                        - Get you boxes data from MassDumper, using Reddit's format. <br>
+                        - Click Import and a modal will appear, paste the data into the textarea. <br>
+                        - Click Import, your boxes are ready to view. <br>
+                    </p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-sm btn-danger" data-dismiss="modal" aria-hidden="true"><i class="fa fa-times"></i>
+                    Close</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+</aside>
+
+</div>
+<!-- jQuery 2.0.2 -->
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+<!-- jQuery UI 1.10.3 -->
+<script src="public/js/jquery-ui-1.10.3.min.js" type="text/javascript"></script>
+<!-- Bootstrap -->
+<script src="public/js/bootstrap.min.js" type="text/javascript"></script>\
+<!-- AdminLTE App -->
+<script src="public/js/AdminLTE/app.js" type="text/javascript"></script>
+
+<!-- exclusive scripts -->
+<script src="public/js/xytools.js"></script>
+</body>
+</html>
---
+++ public/js/xytools.js
@@ -0,0 +1,92 @@
+// to do: process csv file for detailed information
+// 
+
+(function($, window, document) {
+	var rawdata = '';
+	function process_box_data(str) {
+		$('.show-info img').attr('src', 'public/images/favicon.ico');
+		boxes = str.replace("\n| Box | Slot | Name | Nature | Ability | Spread | SV\n|:--|:--|:--|:--|:--|:--|:--\n", "");
+		temp = boxes.split("\n");
+		for (var i = 0; i < temp.length; i++) {
+			pkm = temp[i];
+			var properties = pkm.split(" | ");
+			properties[0] =properties[0].replace('| ', '')
+			properties[properties.length-1] = properties[properties.length-1].replace(' |', '');
+			box = properties[0];
+			position = properties[1].split(",");
+			species = properties[2];
+			data_species = species.replace(" (M)", "").replace(" (F)", ""); 
+			nature = properties[3];
+			ability = properties[4];
+			spread = properties[5].replace(/31/g, '<b class="text-red">31</b>').replace(/\./g, "-");
+			if (properties[6]) {
+				esv = properties[6];
+			};
+			// console.log(properties);
+			image = '<img src="public/images/minisprites/'+data_species+'.png" alt="">';
+			data = "Nature: "+nature+"<br />"+"Ability: "+ability+"<br />"+"IVs Spread: "+spread;
+			if (properties[6]) {
+				data+="<br />"+"EVS: "+esv;
+				species = '<b class="text-red">[EGG]</b> '+species;
+			};
+			$('#'+box+' #'+position[0]+'-'+position[1]+" button").attr('data-content', data).attr('data-original-title', species).empty().append(image);
+		};
+		
+	}
+	function switchbox(id, name) {
+		currentbox = id;
+		switch (currentbox) {
+			case "#B1":
+				$('.to-prev-box').attr('data-to-box', '#B30');
+				$('.to-next-box').attr('data-to-box', '#B2');
+				break;
+			case "#B30":
+				$('.to-prev-box').attr('data-to-box', '#B29');
+				$('.to-next-box').attr('data-to-box', '#B1');
+				break;
+			default:
+				cbn = currentbox.replace( /^\D+/g, '');
+				$('.to-prev-box').attr('data-to-box', '#B'+(cbn*1-1));
+				$('.to-next-box').attr('data-to-box', '#B'+(cbn*1+1));
+				break;
+		}
+		boxname = 'Box '+currentbox.replace( /^\D+/g, '');
+		$('.poke-box:visible').hide();
+		$(id).show();
+		$('.box-title').text(boxname);
+		$('.box-num').text(id);
+	}
+	$(function() {
+		if (localStorage.getItem("boxesdata")) {
+			process_box_data(localStorage.getItem("boxesdata"));
+		};
+		$('#cim').on('click', function () {
+			$('#import-modal').modal('show');
+		});
+		$('#cfm').on('click', function () {
+			$('#faqs-modal').modal('show');
+		});
+		$('.show-info').popover({
+            animation: true,
+            placement: 'top',
+            selector: false,
+            trigger: 'hover',
+            delay: 0,
+            html: true,
+        });
+		$('#impd').on('click', function() {
+			rawdata = $('#import-modal textarea').val();
+			localStorage.setItem('boxesdata', rawdata);
+			process_box_data(rawdata);
+			$('#import-modal').modal('hide');
+		});
+		$('.box-list li a').on('click', function () {
+			id = $(this).attr('href');
+			switchbox(id);
+		});
+		$('.switch-box').on('click', function () {
+			id = $(this).attr('data-to-box');
+			switchbox(id);
+		});
+	});
+}(window.jQuery, window, document));
